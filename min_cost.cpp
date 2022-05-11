2a2cba873a201b5f440d76c96c19c5be03932e22
adamr250 <adamr250@interia.pl>
2022-05-11 15:34:40 +0200

    Wczytywanie wejscia

--- min_cost.cpp
+++ min_cost.cpp
@@ -1,71 +1,109 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    int tmp, add;
+    int tmp;
     int fieldNum, breweryNum, innNum, crossNum, roadNum;
-    int flow, cost, maks;
-    string x, y;
+    int maks;
+    string roadStart, roadEnd, flow, cost;
+
     ifstream in("inCost.txt");
     in >> fieldNum >> breweryNum >> innNum >> crossNum >> roadNum;
     maks = fieldNum + breweryNum + crossNum + innNum;
+
     int fieldProduction[fieldNum], innCapacity[innNum], flowMatrix[maks][maks], costMatrix[maks][maks];
+    for(int i = 0; i<maks; i++) {
+        for(int j=0; j<maks; j++) {
+            flowMatrix[i][j] = 0;
+            costMatrix[i][j] = 0;
+        }
+    }
+
     for (int i = 0; i < fieldNum; i++)
     {
         in >> tmp;
         fieldProduction[i] = tmp;
-        cout << tmp << endl;
+        //flowMatrix[0]
+        //cout << tmp << endl;
     }
     for (int i = 0; i < innNum; i++)
     {
         in >> tmp;
         innCapacity[i] = tmp;
-        cout << tmp << endl;
+        //cout << tmp << endl;
     }
+    for(int i = 0; i<maks; i++) {
+        for(int j=0; j<maks; j++) {
+            cout << flowMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "--------------------" << endl;
     for (int i = 0; i < roadNum; i++)
     {
-        in >> x >> y >> flow >> cost;
-        switch (x[0])
+        int addX=0, addY=0;
+        in >> roadStart >> roadEnd >> flow >> cost;
+        switch (roadStart[0])
         {
-        case 'p':
-        {
-            break;
-        }
-        case 's':
-        {
-            break;
-        }
-        case 'b':
-        {
-            break;
-        }
-
-        default:
-            break;
+            case 's':
+            {
+                addX = fieldNum;
+                break;
+            }
+            case 'b':
+            {
+                addX = fieldNum + crossNum;
+                break;
+            }
         }
 
-        switch (y[0])
+        switch (roadEnd[0])
         {
-        case 's':
-        {
-            break;
+            case 's':
+            {
+                addY = fieldNum;
+                break;
+            }
+            case 'b':
+            {
+                addY = fieldNum + crossNum;
+                break;
+            }
+            case 'k':
+            {
+                addY = fieldNum + crossNum + breweryNum;
+                break;
+            }
         }
-        case 'b':
-        {
-            break;
-        }
-        case 'k':
-        {
-            break;
-        }
-
-        default:
-            break;
-        }
+        int x = addX + stoi(roadStart.erase(0, 1));
+        int y = addY + stoi(roadEnd.erase(0, 1));
+        flowMatrix[x][y] = stoi(flow);
+        //costMatrix[x][y] = stoi(cost);
+        if(x==0 || y==0)
+            cout << "ZERO" <<endl;
+        //cout << x << " " << y << " " << stoi(flow) << " " << stoi(cost) << endl;
+        //cout << flowMatrix[x][y] << endl;
     }
+    for(int i = 0; i<maks; i++) {
+        for(int j=0; j<maks; j++) {
+            cout << flowMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    for(int i = 0; i<maks; i++) {
+        int tmp1 = flowMatrix[0][i];
+        int tmp2 = flowMatrix[i][0];
+        if(tmp1 != 0)
+            cout << tmp1 << " ";
+        if(tmp2 != 0)
+            cout << tmp2 << " ";
 
+    }
+    cout << endl;
+    //cout << flowMatrix[11][0] << endl;
     return 0;
 }
