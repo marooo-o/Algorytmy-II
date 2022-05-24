f75fb15bcd34e4d4fa974a4d430826bf58716293
adamr250 <102742989+adamr250@users.noreply.github.com>
2022-05-24 23:40:10 +0200

    Nowy algorytm

--- minCost.cpp
+++ minCost.cpp
@@ -1,111 +1,157 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include<iostream>
+#include<climits>
+#include<vector>
 using namespace std;
 
-struct output {
-    int cost;
-    int flow;
-};
-
-int N;
-int INF	= INT_MAX / 2 - 1;
-vector<vector<int>> cap, cost;
+int graphSize, minCost = 0, addCost, minusCost;
 vector<bool> found;
-vector<int> dist, dad, pi;
-vector<vector<int>> flow;
+vector<int> dist, d;
+vector<vector<int>> cap, cost;
+vector<vector<bool>> visited;
 
-bool searchGraph(int src, int sink) {
+int miniDist(vector<int> dist, vector<bool> found) // finding minimum distance
+{
+    int minimum=INT_MAX,ind;
+
+    for(int k=0;k<graphSize;k++) {
+        if(found[k]==false && dist[k]<=minimum) {
+            minimum=dist[k];
+            ind=k;
+        }
+    }
+    return ind;
+}
+
+void DijkstraAlgo(vector<vector<int>> cost, int src, bool print) // adjacency matrix
+{
+    //int dist[graphSize]; // // array to calculate the minimum distance for each node
+    //bool found[graphSize];// boolean array to mark visited and unvisited for each node
+    d.clear();
     found.assign(found.size(), false);
-    dist.assign(dist.size(), INF);
-    dist[src] = 0;
-    while (src != N) {
-        int best = N;
-        found[src] = true;
-        for (int k = 0; k < N; k++) {
-            if (found[k])
-                continue;
-            if (flow[k][src] != 0) {
-                int val = dist[src] + pi[src] - pi[k] - cost[k][src];
-                if (dist[k] > val) {
-                    dist[k] = val;
-                    dad[k] = src;
-                }
-            }
+    dist.assign(dist.size(), INT_MAX);
 
-            if (flow[src][k] < cap[src][k]) {
-                int val = dist[src] + pi[src] - pi[k] + cost[src][k];
-                if (dist[k] > val) {
-                    dist[k] = val;
-                    dad[k] = src;
-                }
-            }
-            if (dist[k] < dist[best])
-                best = k;
-        }
-        src = best;
-    }
-    for (int k = 0; k < N; k++)
-        pi[k] = min(pi[k] + dist[k], INF);
-    return found[sink];
-}
+    addCost = 0;
+    minusCost = 0;
+    int current = cost.size()-1;
+    /*for(int k = 0; k<graphSize; k++) {
+        dist[k] = INT_MAX;
+        found[k] = false;
+    }*/
 
-int getMaxFlow(int src, int sink) {
-    output o;
+    dist[src] = 0;   // Source vertex distance is set 0
 
-	int totflow = 0, totcost = 0;
-
-	while (searchGraph(src, sink)) {
-		int amt = INF;
-        for (int x = sink; x != src; x = dad[x])
-        amt = min(amt, flow[x][dad[x]] != 0 ? flow[x][dad[x]] : cap[dad[x]][x] - flow[dad[x]][x]);
-        for (int x = sink; x != src; x = dad[x]) {
-            if (flow[x][dad[x]] != 0) {
-                flow[x][dad[x]] -= amt;
-                totcost -= amt * cost[x][dad[x]];
-            } else {
-                flow[dad[x]][x] += amt;
-                totcost += amt * cost[dad[x]][x];
+    for(int k = 0; k<graphSize; k++) {
+        int m=miniDist(dist,found);
+        //cout << "m: " << m << endl;
+        found[m]=true;
+        for(int k = 0; k<graphSize; k++) {
+            // updating the distance of neighbouring vertex
+            if(!(found[k]) && (cost[m][k]) && (dist[m]!=INT_MAX) && (dist[m]+cost[m][k]<dist[k])) {
+                dist[k]=dist[m]+cost[m][k];
+                d[k]=m;
+                if(print)
+                    cout << m << ", " << k << ", " << dist[k] << " : " << dist[m] << "\t\t" << cost[m][k] << endl;
             }
         }
-        totflow += amt;
+    }
+    /*cout<<"Vertex\tDistance from source vertex"<<endl;
+    for(int k = 0; k<graphSize; k++) {
+        //char str=65+k;
+        cout<<k<<"\t"<<dist[k]<<endl;
+    }*/
+
+///dodawanie kosztu tylko tych sciezek, ktorych jeszcze nie odwiedzielismy
+    while(current) {
+        //cout << d[current] << "-" << current << " : " << cap[d[current]][current] << endl;
+        if(!visited[d[current]][current]) {
+            visited[d[current]][current] = true;
+            addCost += cost[d[current]][current];
+//usuwanie nadmiarowego kosztu, chyba ze znajde sposob aby algorytm nie ignorowal sciezek o koszcie 0
+            if(!d[current]) {
+                addCost--;
+                //minusCost++;
+                //cout << "source" << endl;
+            }
+            if(current == cost.size()-1) {
+                addCost--;
+                //minusCost++;
+                //cout << "sink" << endl;
+            }
+
+//---------------------------------------------------------------------------------------------------
+        }
+        current = d[current];
     }
 
-    return totcost;
+    minCost += addCost;
+    cout << "cost: " << minCost << "; addCost: " << addCost << endl;
 }
 
-int main() {
-    int s = 0, t = 4;
-            //0   1   2   3   4   5   6   7   8
-    cap = { { 0,  10, 17, 0,  0,  0,  0,  0,  0 }, //0
-            { 0,  0,  0,  3,  1,  0,  0,  0,  0 }, //1
-            { 0,  0,  0,  6,  0,  4,  0,  0,  0 }, //2
-            { 0,  0,  0,  0,  4,  3,  0,  0,  0 }, //3
-            { 0,  0,  0,  0,  0,  3,  4,  9,  0 }, //4
-            { 0,  0,  0,  0,  0,  0,  8,  7,  0 }, //5
-            { 0,  0,  0,  0,  0,  0,  0,  0,  7 }, //6
-            { 0,  0,  0,  0,  0,  0,  0,  0,  15}, //7
-            { 0,  0,  0,  0,  0,  0,  0,  0,  0 }};//8
+int main()
+{
+			//0   1   2   3   4   5   6   7
+    cap = { { 0,  4,  4,  0,  0,  0,  0,  0 },
+            { 0,  0,  0,  5,  6,  0,  0,  0 },
+            { 0,  0,  0,  5,  0,  0,  0,  0 },
+            { 0,  0,  0,  0,  0,  5,  2,  0 },
+            { 0,  0,  0,  0,  0,  0,  5,  0 },
+            { 0,  0,  0,  0,  0,  0,  0,  5 },
+            { 0,  0,  0,  0,  0,  0,  0,  2 },
+            { 0,  0,  0,  0,  0,  0,  0,  0 }};
 
-            //0   1   2   3   4   5   6   7   8
-    cost = {{ 0,  0,  0,  0,  0,  0,  0,  0,  0 }, //0
-            { 0,  0,  0,  2,  1,  0,  0,  0,  0 }, //1
-            { 0,  0,  0,  1,  0,  2,  0,  0,  0 }, //2
-            { 0,  0,  0,  0,  5,  1,  0,  0,  0 }, //3
-            { 0,  0,  0,  0,  0,  3,  9,  4,  0 }, //4
-            { 0,  0,  0,  0,  0,  0,  5,  4,  0 }, //5
-            { 0,  0,  0,  0,  0,  0,  0,  0,  0 }, //6
-            { 0,  0,  0,  0,  0,  0,  0,  0,  0 }, //7
-            { 0,  0,  0,  0,  0,  0,  0,  0,  0 }};//8
+			//0   1   2   3   4   5   6   7
+    cost = {{ 0,  2,  3,  0,  0,  0,  0,  0 },
+            { 0,  0,  0,  2,  5,  0,  0,  0 },
+            { 0,  0,  0,  3,  0,  0,  0,  0 },
+            { 0,  0,  0,  0,  0,  2,  4,  0 },
+            { 0,  0,  0,  0,  0,  0,  3,  0 },
+            { 0,  0,  0,  0,  0,  0,  0,  4 },
+            { 0,  0,  0,  0,  0,  0,  0,  1 },
+            { 0,  0,  0,  0,  0,  0,  0,  0 }};
 
-    N = cap.size();
-    found = vector<bool> (N, 0);
-    flow = vector<vector<int>> (N, vector<int>(N) );
-    dist = vector<int> (N+1);
-    dad = vector<int> (N);
-    pi = vector<int> (N);
-    int wynik = getMaxFlow(s, t);
+    graphSize = cost.size();
+    d = vector<int> (graphSize);
+    found = vector<bool> (graphSize, 0);
+    dist = vector<int> (graphSize);
+    visited = vector<vector<bool>> (graphSize, vector<bool>(graphSize));
 
-    printf("cost: %d\n", wynik);
+    //cout << "\n\n" << endl;
+    while(1) {
+        DijkstraAlgo(cost, 0, 0);
+
+
+        int minFlowValue = INT_MAX;
+        int current = cost.size()-1;
+        while(current) {
+            cout << d[current] << "-" << current << " : " << cap[d[current]][current] << endl;
+            if(cap[d[current]][current] < minFlowValue) {
+                minFlowValue = cap[d[current]][current];
+            }
+            /*if(current == 0) {
+                //cout << endl;
+                break;
+            }*/
+            current = d[current];
+        }
+        //cout << "\n" << minFlowValue << "\n" << endl;
+        current = cost.size()-1;
+        while(current) {
+            cap[d[current]][current] -= minFlowValue;
+            //cout << d[current] << "-" << current << " : " << cap[d[current]][current] << endl;
+            if(cap[d[current]][current] == 0) {
+                cost[d[current]][current] = 0;
+                //cout << d[current] << ", " << current << " : " << cost[d[current]][current] << endl;
+            }
+            current = d[current];
+        }
+        if(addCost == 0) {
+            break;
+        }/* else {
+            cout << "HERE" << endl;
+            minCost -= minusCost;
+        }*/
+    }
+
+    cout << "MIN COST: " << minCost << endl;
+    return 0;
 }
